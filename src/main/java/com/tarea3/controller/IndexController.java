d50d799f1e5be82f9138f12b33699b06df857aa2
AdriánRicardo <adrianos1802@gmail.com>
2022-03-06 16:59:43 -0600

    Plantilla y controller terminado para las páginas principal y de contactenos

--- src/main/java/com/tarea3/controller/IndexController.java
+++ src/main/java/com/tarea3/controller/IndexController.java
@@ -1,21 +1,28 @@
-
 package com.tarea3.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 //ESTA CLASE SE HACE CON EL SPRING FRAMEWORK, ES UNA CLASE ESPECIAL Y SE NECESITA PARA USAR LOS TEMPLATES.
-
 @Controller
 @Slf4j
 public class IndexController {
-    
-    @RequestMapping("/")
-    public String page(Model model) {
-        model.addAttribute("Arquitectura MVC");
+
+    //CON ESTO SE INDICA QUE COMIECE EL INDEX DEL TAMPLETE
+    @GetMapping("/")
+    public String inicio(Model model) {
+        log.info("Arquitectura MVC");
         return "index";
     }
-    
+
+    //SE INDICA QUE SE USARÁ EL TAMPLATE CON ESE NOMBRE
+    @GetMapping("/contactenos")
+    public String cotactenos(Model model) {
+        log.info("Arquitectura MVC");
+        return "contactenos";
+    }
+
+
 }
